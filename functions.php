19b36f9522f52142f263821b1e317f71f2fca166
OttavioM <ottavio.mazzaretto@gmail.com>
2023-08-14 20:10:06 +0200

    functions.php'now the time is good and it can be used from every nation, take out the tie zone and the hour

--- functions.php
+++ functions.php
@@ -190,7 +190,7 @@ function greet_user( $atts ) {
     } elseif ( $current_hour >= 12 && $current_hour < 18 ) {
         $greeting = 'Good afternoon';
 	} elseif ( $current_hour >= 23 || $current_hour < 5 ) {
-        $greeting = 'Good night' .$time_zone .$current_hour;
+        $greeting = 'Good night';
 	} elseif ( $current_hour >= 18 && $current_hour < 23 ) {
         $greeting = 'Good evening';
     } else {
