7ac8d0eb4f8364fc8f6a39e907b622939b1b34a0
OttavioM <ottavio.mazzaretto@gmail.com>
2023-10-20 20:24:16 +0200

    desesperate otherwise I quit

--- js/carousel_hommepage_products.js
+++ js/carousel_hommepage_products.js
@@ -33,11 +33,12 @@
     // }
 // }
 
-// window.onload = function() {
+window.onload = function() {
     // JavaScript code goes here
     var ulElement = document.getElementById('metaslider-id-640').querySelector('ul.slides');
     var liElements = ulElement.getElementsByTagName('li');
   
+    ulElement.style.width = '100%';
     console.log('ulElement:', ulElement); // Log the ul element
     console.log('ul.slide width is: ' + ulElement.offsetWidth)
   
@@ -56,16 +57,16 @@
     console.log('Total width including spaces: ' + totalWidth + 'px');
     console.log('JS with DOM');
   
-    // Now, check if the ul.slides width is greater than totalWidth
-    // if (ulElement.offsetWidth/10 > totalWidth) {
-    //   ulElement.style.display = 'flex';
-    //   ulElement.style.justifyContent = 'center'; // Add justify-content: center
-    //   ulElement.style.width = '100%';
-    // } else {
-    //   ulElement.style.display = 'block'; // Revert to the default value if it's not greater
-    //   ulElement.style.justifyContent = ''; // Revert justify-content if not greater
-    //   ulElement.style.width = '1000%'; // as default
-    //   console.log('here we have ulElement.offsetWidth < totalWidth');
-    // }
-//   };
+    Now, check if the ul.slides width is greater than totalWidth
+    if (ulElement.offsetWidth/10 > totalWidth) {
+      ulElement.style.display = 'flex';
+      ulElement.style.justifyContent = 'center'; // Add justify-content: center
+      ulElement.style.width = '100%';
+    } else {
+      ulElement.style.display = 'block'; // Revert to the default value if it's not greater
+      ulElement.style.justifyContent = ''; // Revert justify-content if not greater
+      ulElement.style.width = '1000%'; // as default
+      console.log('here we have ulElement.offsetWidth < totalWidth');
+    }
+  };
   
