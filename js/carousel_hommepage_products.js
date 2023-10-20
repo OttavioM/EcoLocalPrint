678711eeb955af911c27587238cfccb67fb243ce
OttavioM <ottavio.mazzaretto@gmail.com>
2023-10-20 19:27:05 +0200

    Update carousel_hommepage_products.js

--- js/carousel_hommepage_products.js
+++ js/carousel_hommepage_products.js
@@ -1,30 +1,32 @@
 //js/carousel_hommepage_products.js
 
-// JavaScript code goes here
-var ulElement = document.getElementById('metaslider-id-640').querySelector('ul.slides');
-var liElements = ulElement.getElementsByTagName('li');
+document.addEventListener('DOMContentLoaded', function() {
+    // JavaScript code goes here
+    var ulElement = document.getElementById('metaslider-id-640').querySelector('ul.slides');
+    var liElements = ulElement.getElementsByTagName('li');
 
-console.log('ulElement:', ulElement); // Log the ul element
+    console.log('ulElement:', ulElement); // Log the ul element
 
-var totalWidth = 0;
+    var totalWidth = 0;
 
-for (var i = 0; i < liElements.length; i++) {
-  totalWidth += liElements[i].offsetWidth; // Width of the li element
-//   if (i < liElements.length - 1) {
-//     // Add space between elements (margin-right)
-//     totalWidth += parseInt(window.getComputedStyle(liElements[i]).marginRight, 10);
-//   }
-  
-  console.log('liElement ' + i + ':', liElements[i]); // Log each li element
-}
+    for (var i = 0; i < liElements.length; i++) {
+    totalWidth += liElements[i].offsetWidth; // Width of the li element
+    if (i < liElements.length - 1) {
+        // Add space between elements (margin-right)
+        totalWidth += parseInt(window.getComputedStyle(liElements[i]).marginRight, 10);
+    }
+    
+    console.log('liElement ' + i + ':', liElements[i]); // Log each li element
+    }
 
-console.log('Total width including spaces: ' + totalWidth + 'px');
-
-// Now, check if the ul.slides width is greater than totalWidth
-if (ulElement.offsetWidth > totalWidth) {
-  ulElement.style.display = 'flex';
-  ulElement.style.justifyContent = 'center'; // Add justify-content: center
-} else {
-  ulElement.style.display = 'block'; // Revert to the default value if it's not greater
-  ulElement.style.justifyContent = ''; // Revert justify-content if not greater
+    console.log('Total width including spaces: ' + totalWidth + 'px');
+    console.log('JS with DOM')
+    // Now, check if the ul.slides width is greater than totalWidth
+    if (ulElement.offsetWidth > totalWidth) {
+    ulElement.style.display = 'flex';
+    ulElement.style.justifyContent = 'center'; // Add justify-content: center
+    } else {
+    ulElement.style.display = 'block'; // Revert to the default value if it's not greater
+    ulElement.style.justifyContent = ''; // Revert justify-content if not greater
+    }
 }
