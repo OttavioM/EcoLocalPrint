fecbb10d79e5f1151b18d1aa803a7f834d9133bb
OttavioM <ottavio.mazzaretto@gmail.com>
2023-10-20 18:58:35 +0200

    functions.php- enqueue the my_custom_js

--- functions.php
+++ functions.php
@@ -212,4 +212,10 @@ function greet_user( $atts ) {
 }
 add_shortcode( 'greet_user', 'greet_user' );
 
+function my_custom_scripts() {
+    wp_enqueue_script( 'carousel_hommepage_products', get_stylesheet_directory_uri() . '/js/carousel_hommepage_products.js', array( 'jquery' ),'',true );
+}
+add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
+
+// END OF THE PHP
 ?>
