9700f45a6dc8cd24ae083f6dd8890819d7b2da85
OttavioM <ottavio.mazzaretto@gmail.com>
2023-10-20 20:17:44 +0200

    Update carousel_hommepage_products.js

--- js/carousel_hommepage_products.js
+++ js/carousel_hommepage_products.js
@@ -57,15 +57,15 @@
     console.log('JS with DOM');
   
     // Now, check if the ul.slides width is greater than totalWidth
-    if (ulElement.offsetWidth/10 > totalWidth) {
-      ulElement.style.display = 'flex';
-      ulElement.style.justifyContent = 'center'; // Add justify-content: center
-      ulElement.style.width = '100%';
-    } else {
-      ulElement.style.display = 'block'; // Revert to the default value if it's not greater
-      ulElement.style.justifyContent = ''; // Revert justify-content if not greater
-      ulElement.style.width = '1000%'; // as default
-      console.log('here we have ulElement.offsetWidth < totalWidth');
-    }
+    // if (ulElement.offsetWidth/10 > totalWidth) {
+    //   ulElement.style.display = 'flex';
+    //   ulElement.style.justifyContent = 'center'; // Add justify-content: center
+    //   ulElement.style.width = '100%';
+    // } else {
+    //   ulElement.style.display = 'block'; // Revert to the default value if it's not greater
+    //   ulElement.style.justifyContent = ''; // Revert justify-content if not greater
+    //   ulElement.style.width = '1000%'; // as default
+    //   console.log('here we have ulElement.offsetWidth < totalWidth');
+    // }
 //   };
   
