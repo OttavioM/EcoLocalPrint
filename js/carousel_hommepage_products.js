d3be3ad53b224c42758a3e53ba3ce846440b138b
OttavioM <ottavio.mazzaretto@gmail.com>
2023-10-20 19:09:53 +0200

    Update carousel_hommepage_products.js

--- js/carousel_hommepage_products.js
+++ js/carousel_hommepage_products.js
@@ -1,7 +1,7 @@
 //js/carousel_hommepage_products.js
 
 // JavaScript code goes here
-var ulElement = document.getElementById('#metaslider-id-640 ul.slides');
+var ulElement = document.getElementById('metaslider-id-640').querySelector('ul.slides');
 var liElements = ulElement.getElementsByTagName('li');
 
 var totalWidth = 0;
@@ -18,9 +18,9 @@ console.log('Total width including spaces: ' + totalWidth + 'px');
 
 // Now, check if the ul.slides width is greater than totalWidth
 if (ulElement.offsetWidth > totalWidth) {
-    ulElement.style.display = 'flex';
-    ulElement.style.justifyContent = 'center'; // Add justify-content: center
-  } else {
-    ulElement.style.display = 'block'; // Revert to the default value if it's not greater
-    ulElement.style.justifyContent = ''; // Revert justify-content if not greater
-  }
+  ulElement.style.display = 'flex';
+  ulElement.style.justifyContent = 'center'; // Add justify-content: center
+} else {
+  ulElement.style.display = 'block'; // Revert to the default value if it's not greater
+  ulElement.style.justifyContent = ''; // Revert justify-content if not greater
+}
