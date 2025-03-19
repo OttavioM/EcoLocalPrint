f6f664eb481286de3b0e599b3897a68b4cbce87b
OttavioM <ottavio.mazzaretto@gmail.com>
2025-03-19 14:48:18 +0100

    functinos.php - thirt icon

--- functions.php
+++ functions.php
@@ -72,7 +72,7 @@ function ds_custom_wc_empty_cart_text()
             <p align="center" class="empty-cart-icon"><i class="bi bi-cart"></i></p>
         </div>
 		<div class="empty-cart">
-			<h2>Naked cart alert! Time to dress it up with some cool fashion. </h2>
+			<h2>👕Naked cart alert! Time to dress it up with some cool fashion. </h2>
 			<p> Did you know? Every purchase helps restore habitats<i class="fa-solid fa-otter"></i>. Your cart is waiting to do some good! </p>
 		</div>
 	</div>';
